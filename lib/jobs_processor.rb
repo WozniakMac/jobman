cacdf300ced56c05c9243444859a25cbe2395742
Maciej Wozniak <maciekwo@gmail.com>
2018-07-22 11:25:55 +0100

    Add dependency processor.

--- lib/jobs_processor.rb
+++ lib/jobs_processor.rb
@@ -1,7 +1,30 @@
 # frozen_string_literal: true
 
+require 'active_support/all'
+
 class JobsProcessor
-  def self.process(jobs_list)
-    jobs_list
+  def process(jobs_list)
+    @jobs_list = jobs_list
+    @processed_jobs = []
+
+    while @jobs_list.count.positive?
+      job = @jobs_list.keys.first
+      process_job(job)
+    end
+
+    @processed_jobs.join
+  end
+
+  private
+
+  def process_job(job)
+    dependency = @jobs_list[job]
+
+    if dependency.present? && @jobs_list.keys.include?(dependency)
+      process_job(dependency)
+    end
+
+    @processed_jobs << job
+    @jobs_list.delete(job)
   end
 end
--- spec/lib/jobs_processor_spec.rb
+++ spec/lib/jobs_processor_spec.rb
@@ -4,16 +4,41 @@ require 'spec_helper'
 require_relative '../../lib/jobs_processor'
 
 describe JobsProcessor do
+  subject(:jobs_processor) { described_class.new }
+
   describe '.process' do
     context 'when job list is empty' do
       it 'returns empty list' do
-        expect(described_class.process('')).to eq('')
+        expect(jobs_processor.process({})).to eq('')
       end
     end
 
     context 'when we pass jobs list without dependeicies' do
+      let(:jobs_list) do
+        {
+          'a' => nil,
+          's' => nil,
+          'd' => nil,
+          'f' => nil
+        }
+      end
+
       it 'returns this same list' do
-        expect(described_class.process('asdf')).to eq('asdf')
+        expect(jobs_processor.process(jobs_list)).to eq('asdf')
+      end
+    end
+
+    context 'when we pass jobs with one dependency' do
+      let(:jobs_list) do
+        {
+          'a' => nil,
+          'b' => 'c',
+          'c' => nil
+        }
+      end
+
+      it 'returns this same list' do
+        expect(jobs_processor.process(jobs_list)).to eq('acb')
       end
     end
   end
